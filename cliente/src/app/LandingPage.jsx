fc526d237f2348627fffd6972bac34e9707f537f
nahir2334github <nairf2895@gmail.com>
2025-11-03 19:20:27 -0300

    agrego cosas

--- cliente/src/app/LandingPage.jsx
+++ cliente/src/app/LandingPage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Hero from "../components/Hero";
 import MenuPreview from "../components/MenuPreview";
 import ReservasPreview from "../components/ReservasPreview";
